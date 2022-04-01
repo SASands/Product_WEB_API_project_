279b0e746134a5a5d6ebe0e81548119840cd648a
SASands <snads@live.com>
2022-04-01 13:00:13 -0400

    updated views file, added product detail to respond when requested by PK/ID + fixed syntax error in views file

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.products_list),
-    path('<int:pk>/', views.products_list),
+    path('<int:pk>/', views.product_detail),
+
 
 ]
--- products/views.py
+++ products/views.py
@@ -9,21 +9,27 @@ from .models import Products
 @api_view(['Get', 'POST'])
 def products_list(request):
 
-    if request.method == 'PUT':
-
-
     if request.method == 'GET':
         products = Products.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-    
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    try:
+        product = Products.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)        
+        
+    except Products.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
 
 
 
